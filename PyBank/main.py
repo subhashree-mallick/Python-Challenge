a50a425ae7a5a5de0cd536fafd94fdef68c828ce
subhashree-mallick <74946482+subhashree-mallick@users.noreply.github.com>
2021-01-21 21:29:43 -0500

    Update main.py

---
+++ PyBank/main.py
@@ -0,0 +1,59 @@
+#Dependencies
+import os
+import csv
+
+#Intial Variables
+Total_number_of_month=[]
+total_profitloss=[]
+net_profitloss=[]
+greatest_increase_in_profit=["", 0]
+greatest_decrease_in_profit=["",999999999]
+total_amount=0
+
+#files path associated with load and output
+csvpath = os.path.join('Resources','budget_data.csv')
+outputpath= os.path.join('Analysis','budget_analysis.txt')
+
+
+#Open file in read mode
+with open (csvpath) as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=',')
+    #Skip the header
+    csv_header=next(csvreader)
+    print(f"csv hearder: {csv_header} ")
+    #
+    first_row=next(csvreader)
+    prev_net=int(first_row[1])
+    
+
+    #Read each row of data.
+    for row in csvreader:
+        Total_number_of_month.append(row[0])
+        total_amount=total_amount+int(row[1])
+        #calculation for net change over the entire period
+        net_change=int(row[1])-prev_net
+        prev_net=int(row[1])
+        net_profitloss.append(net_change)
+        #calculation for greatest increase in profit
+        if net_change > greatest_increase_in_profit[1]:
+           greatest_increase_in_profit[0]=row[0]
+           greatest_increase_in_profit[1]=net_change
+        #calculation for greatest decrease in profit
+        if net_change < greatest_decrease_in_profit[1]:
+           greatest_decrease_in_profit[0]=row[0]
+           greatest_decrease_in_profit[1]=net_change   
+
+
+    #calculate the average net change over a period
+    net_monthly_change=sum(net_profitloss) /len(net_profitloss) 
+
+
+with open(outputpath,"W") as txt_file:
+    txt_file.write(
+        print(f"Total number of month : {len(Total_number_of_month)} ")
+        print("Total profit/loss :", total_amount)
+        print(f"Average of monthly changes : {net_monthly_change:.2f}")
+        print(f"greatest increase in profits : {greatest_increase_in_profit[0]} (${greatest_increase_in_profit[1]})")
+        print(f"greatest decrease in profits : {greatest_decrease_in_profit[0]} (${greatest_decrease_in_profit[1]})"))
+    
+   
